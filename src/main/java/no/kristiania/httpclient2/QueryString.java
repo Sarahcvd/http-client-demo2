1a49a6410618c4e3f68806019a5e86088176d20c
sarahcvd <sarahmurstad@hotmail.com>
2020-10-08 13:35:47 +0200

    ShouldRetrieveQueryParameter test failing

---
+++ src/main/java/no/kristiania/httpclient2/QueryString.java
@@ -0,0 +1,11 @@
+package no.kristiania.httpclient2;
+
+public class QueryString {
+    public QueryString(String queryString) {
+
+    }
+
+    public String getParameter(String name) {
+        return null;
+    }
+}
