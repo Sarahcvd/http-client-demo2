ce9b56fa811da87ff143b4207b2cf3de63b32329
Waligustav <wgbjork@gmail.com>
2020-10-13 10:14:11 +0200

    Http server should return http headers (failing) new branch created

--- src/test/java/no/kristiania/httpclient2/HttpServerTest.java
+++ src/test/java/no/kristiania/httpclient2/HttpServerTest.java
@@ -8,16 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HttpServerTest {
     @Test
-    void shouldReturnSuccessfulErrorCode() throws IOException {
-        HttpServer server = new HttpServer(10001);
+    void shouldReturnSuccessfulStatusCode() throws IOException {
+        new HttpServer(10001);
         HttpClient client = new HttpClient("localhost", 10001, "/echo");
         assertEquals(200, client.getStatusCode());
     }
 
     @Test
-    void shouldReturnUnsuccessfulErrorCode() throws IOException {
-        HttpServer server = new HttpServer(10002);
+    void shouldReturnUnsuccessfulStatusCode() throws IOException {
+        new HttpServer(10002);
         HttpClient client = new HttpClient("localhost", 10002, "/echo?status=404");
         assertEquals(404, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnHttpHeaders() throws IOException {
+        new HttpServer(10003);
+        HttpClient client = new HttpClient("localhost", 10003, "/echo?body=HelloWorld");
+        assertEquals("10", client.getResponseHeader("Content-Length"));
     }
 }
